a6cbf883ce0026821ecc7bda2078f21e7583a42b
nazma98 <nazmasarker1311@gmail.com>
2025-02-07 19:35:42 +0600

    Add education, skills components under component directory

---
+++ src/components/Education.jsx
@@ -0,0 +1,89 @@
+export function Education() {
+    return (
+      <section
+      id="education"
+      className="m-8 md:m-12 md:p-10 lg:w-[60%] lg:m-auto"
+    >
+      <div className="flex flex-col items-center justify-center gap-2 mb-5 lg:flex-row lg:gap-8">
+        <h2 className="font-semibold text-2xl text-[#00b1b1] lg:text-3xl">
+          Education
+        </h2>
+        <div className="bg-[#00b1b1] w-[50%] h-0.5 lg:w-[700px] lg:h-[1px]"></div>
+      </div>
+      <div className=" flex flex-col gap-5 w-[80%] m-auto py-5 lg:items-center">
+        <div
+          className="bg-[linear-gradient(135deg,#00b1b169,rgba(248,85,26,0.514))]  shadow-cyan-500/30
+                    shadow-lg rounded-md p-5 transition-all duration-300 active:shadow-xl active:scale-110
+                    lg:hover:shadow-xl lg:hover:scale-105 lg:w-[75%]"
+        >
+          <h3 className="text-xl font-semibold text-[#00b1b1]">
+            MSC in CSE
+          </h3>
+          <p className="text-lg text-white font-medium">
+            Jahangirnagar University, 2023 - 2024
+          </p>
+          <p className="text-sm mt-2 text-justify">
+            Specialized in
+            <ul className="text-sm mt-2 text-justify list-disc p-2">
+              <li>Digital Forensics</li>
+              <li>Federated Learning</li>
+              <li>Natural Language Processing</li>
+              <li>Machine Learning</li>
+              <li>Distributed Database</li>
+              <li>Medical Imaging</li>
+              <li>Computer Vision</li>
+            </ul>
+          </p>
+          <p className="text-sm mt-2 text-justify">
+            CGPA of 1st semester 3.85/4.0
+          </p>
+        </div>
+  
+        <div
+          className="bg-[linear-gradient(135deg,#00b1b169,rgba(248,85,26,0.514))]  shadow-cyan-500/30 
+                    shadow-lg rounded-md p-5 transition-all duration-300 active:shadow-xl active:scale-110
+                    lg:hover:shadow-xl lg:hover:scale-105 lg:w-[75%]"
+        >
+          <h3 className="text-xl font-semibold text-[#00b1b1]">
+            BSC. Hons in CSE
+          </h3>
+          <p className="text-lg text-white font-medium">
+            Jahangirnagar University, 2018 - 2023
+          </p>
+          <p className="text-sm mt-2 text-justify">
+            Focused on
+            <ul className="text-sm mt-2 text-justify list-disc p-2">
+              <li>C & C++ Programming</li>
+              <li>Data Structure</li>
+              <li>Database Management System</li>
+              <li>Object Oriented Programming (Java)</li>
+              <li>Compiler Design (Lex & YACC)</li>
+              <li>Algorithm Design & Analysis</li>
+              <li>System Analysis & Design</li>
+              <li>Software Engineering</li>
+              <li>Software Testing</li>
+              <li>Web Technologies</li>
+              <li>Mobile App Development (Android)</li>
+            </ul>
+          </p>
+          <p className="text-sm mt-2 text-justify">CGPA 3.64/4.00</p>
+        </div>
+  
+        <div
+          className="bg-[linear-gradient(135deg,#00b1b169,rgba(248,85,26,0.514))]  shadow-cyan-500/30 
+                    shadow-lg rounded-md p-5 transition-all duration-300 active:shadow-xl active:scale-110
+                    lg:hover:shadow-xl lg:hover:scale-105 lg:w-[75%]"
+        >
+          <h3 className="text-xl font-semibold text-[#00b1b1]">
+            Higher Secondary Certificate, 2017
+          </h3>
+          <p className="text-lg text-white font-medium">
+            Holy Cross College, 2015 - 2016
+          </p>
+          <p className="text-sm mt-2 text-justify">Science</p>
+          <p className="text-sm mt-2 text-justify">GPA 5.00/5.00</p>
+        </div>
+      </div>
+    </section>
+    );
+  }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,110 @@
+export function Skills() {
+    return (
+      <section
+      id="skills"
+      className="m-8 md:m-12 md:p-10 lg:w-[60%] lg:m-auto"
+    >
+      <div className="flex flex-col items-center justify-center gap-2 mb-5 lg:flex-row lg:gap-8">
+        <h2 className="font-semibold text-2xl text-[#00b1b1] lg:text-3xl">
+          Skills
+        </h2>
+        <div className="bg-[#00b1b1] w-[50%] h-0.5 lg:w-[700px] lg:h-[1px]"></div>
+      </div>
+      <div className="w-[80%] m-auto lg:flex lg:items-center">
+        <div className="flex flex-col text-justify items-center justify-center  mb-6 md:text-xl md:p-8 lg:w-[50%] ">
+          I am Proficient in Python, Java, C, and C++ for problem solving
+          and versatile software development. Also familiar with web
+          frameworks such as Laravel and Django for building robust web
+          applications. I have experiences with Linux for development and
+          scripting.
+        </div>
+        <div
+          className=" bg-[linear-gradient(135deg,#00b1b169,rgba(248,85,26,0.514))]  shadow-cyan-500/30 shadow-md 
+                    rounded-md grid grid-cols-3 place-content-center gap-4 py-5 px-8 
+                    md:w-[60%] md:m-auto lg:grid-cols-4 lg:gap-x-5 lg:gap-y-8 lg:justify-center lg:px-10 lg:py-4 lg:w-[45%]"
+        >
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/HTML5_logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/css-logo-tr.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/js-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/bootstrap-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/c-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/cpp-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/java-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/python-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/django-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/laravel-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/PHP-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/mysql-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/linux-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/typescript-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out  md:w-10 md:h-10 active:bg-[#00b1b1ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/github-logo.png"
+            alt=""
+          />
+          <img
+            className="w-8 h-8 object-contain rounded-md transition-all duration-200 ease-in-out md:w-10 md:h-10 active:bg-[#111313ce] active:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:bg-[#00b1b1ce] lg:hover:[box-shadow:0_0_15px_5px_#00b1b1] lg:hover:scale-110"
+            src="assets/images/tailwind-css_logo.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+    );
+  }
