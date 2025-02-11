f60f41bb81152c7950ac49aca1fc3bb75f4326df
nazma98 <nazmasarker1311@gmail.com>
2025-02-11 12:23:04 +0600

    Add right nav sections in header component

---
+++ src/components/header/RightNavSection.jsx
@@ -0,0 +1,21 @@
+export function RightNavSection(){
+    return (
+        <ul className="flex items-center justify-between gap-3 text-sm md:gap-4 md:text-lg lg:text-xl lg:gap-7">
+        <li className="duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]">
+          <a href="#about">About</a>
+        </li>
+        <li className="duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]">
+          <a href="#education">Education</a>
+        </li>
+        <li className="duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]">
+          <a href="#skills">Skills</a>
+        </li>
+        <li className="duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]">
+          <a href="#experience">Experience</a>
+        </li>
+        <li className="duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]">
+          <a href="#projects">Projects</a>
+        </li>
+      </ul>
+    );
+}
