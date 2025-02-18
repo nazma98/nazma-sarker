c9c12ba2cd4887cdaa821a4192d1c19ab1ec44bb
nazma98 <nazmasarker1311@gmail.com>
2025-02-18 21:24:07 +0600

    Add props and proptypes to render nav items

--- src/components/header/RightNavSection.jsx
+++ src/components/header/RightNavSection.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const navMenu = [
   {
     url: '#about',
@@ -24,15 +26,31 @@ const navMenu = [
 export function RightNavSection() {
   return (
     <>
-      <ul className="flex items-center justify-between gap-3 text-sm md:gap-4 md:text-lg lg:text-xl lg:gap-7">
-        {navMenu.map((navItem) => {
-          return (
-            <li key={navItem.name} className="duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]">
-              <a href={navItem.url}>{navItem.name}</a>
-            </li>
-          );
-        })}
+      <ul className='flex items-center justify-between gap-3 text-sm md:gap-4 md:text-lg lg:text-xl lg:gap-7'>
+        {navMenu.map((navItem) => (
+          <NavMenuItem
+            key={navItem.name}
+            url={navItem.url}
+            name={navItem.name}
+          />
+        ))}
       </ul>
     </>
   );
+}
+
+function NavMenuItem({ url, name }) {
+  return (
+    <li
+      key={navItem.name}
+      className='duration-300 active:scale-105 active:shadow-lg active:text-[#00b1b1] lg:hover:scale-110 lg:hover:text-[#00b1b1] lg:hover:[text-shadow:0_0_15px_#00b1b1]'
+    >
+      <a href={navItem.url}>{navItem.name}</a>
+    </li>
+  );
+}
+
+NavMenuItem.PropTypes = {
+  url: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 }
